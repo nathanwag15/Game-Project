defd1007b90e3dcc6d2564bfac19c60b8885fce2
nathanwagstaff <natewagstaff21@gmail.com>
2020-06-04 19:43:36 -0600

    secondUpdate

---
+++ game.py
@@ -0,0 +1,12 @@
+from random import randrange
+suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs']
+numbers = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King']
+
+def random_selector(suits, numbers):
+    suit = str(suits[randrange(0, 3)])
+    number = str(numbers[randrange(0, 12)])
+    print(number + ' of ' + suit)
+    
+
+
+random_selector(suits, numbers)
